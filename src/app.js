64f43fc5d6fb3ab28d07511f788dd45ae73fcd46
Martalore97 <2lorenzanagarciamarta10@gmail.com>
2023-04-22 19:04:51 +0200

    Starting  with js making id

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
+let apiKey = "13200fbdb70fed1828854131da54a9df";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
