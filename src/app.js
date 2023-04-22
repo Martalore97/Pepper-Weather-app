bd32bec78fb6e498dbbd513e4ef028701f2729d8
Martalore97 <2lorenzanagarciamarta10@gmail.com>
2023-04-22 22:33:28 +0200

    Uploading Humidity and Wind

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  speedElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "13200fbdb70fed1828854131da54a9df";
