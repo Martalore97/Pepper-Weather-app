fb9bc0b1dc22381d901d9ce3ce8266842b1ab3c0
Martalore97 <2lorenzanagarciamarta10@gmail.com>
2023-04-22 22:25:39 +0200

    Uploading humidity HTML and js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 function displayTemperature(response) {
-  console.log(response.data.weather[0]);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "13200fbdb70fed1828854131da54a9df";
