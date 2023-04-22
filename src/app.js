98299b4f59938b4e71f2798762b95072e88379f3
Martalore97 <2lorenzanagarciamarta10@gmail.com>
2023-04-22 19:12:06 +0200

    Upload city

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "13200fbdb70fed1828854131da54a9df";
